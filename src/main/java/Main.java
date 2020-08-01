0afe206a888ac33635144ecee972c42604e34a8c
xiaqiu <1372166673@qq.com>
2020-08-01 12:27:51 +0800

    [20200801]测试Ubuntu下IDEA

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static int main(String[] agrs) {
+        Set<String> result = new HashSet<String>();
+        System.out.println("Hello ubuntu, hello idea!");
+        return 0;
+    }
+}
